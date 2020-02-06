0a7ad5cb8ac25b818430e7ed8c6015103e4b1548
unknown <david.lemos@outlook.com>
2020-02-06 16:57:19 -0300

    integrantes

---
+++ integrantes.js
@@ -0,0 +1,24 @@
+const integrantes = [
+    {
+        nome: "Bruno",
+        passatempo: "Mexer no possante",
+        esporte: "Natação",
+        membrosDoGrupo: ["Henrique", "David", "Bruno"]
+    },
+    {
+        nome: 'Henrique',
+        passatempo: 'Ouvir musica',
+        esporte: 'Canionismo',
+        membrosDoGrupo: ['Henrique', 'Bruno', 'David']
+    },
+    {
+        nome = "David",
+        passatempo = "Netflix",
+        esporte = "Natação",
+        membros = [
+            "Bruno",
+            "Henrique",
+            "David"
+        ]
+    }
+]
